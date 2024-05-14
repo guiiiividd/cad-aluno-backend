7e4dced16dc1462758e6420a2d2669c612a629fc
guiiiividd <guiiiividd@gmail.com>
2024-05-14 11:19:31 -0300

    :sparkles: feat: Configuring and Getting Data from H2 Database

--- src/main/java/com/fatec/student/resources/StudentController.java
+++ src/main/java/com/fatec/student/resources/StudentController.java
@@ -1,40 +1,24 @@
 package com.fatec.student.resources;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fatec.student.entities.Student;
+import com.fatec.student.services.StudentService;
 
 @RestController
 @RequestMapping("students")
 public class StudentController {
-    
+
+    @Autowired
+    private StudentService studentService;
+
     @GetMapping
-    public List<Student> getStudents(){
-        Student s1 = new Student();
-        s1.setId(1);
-        s1.setName("Guilherme");
-        s1.setCourse("Análise e Desenvolvimento de Sistemas");
-
-        Student s2 = new Student();
-        s2.setId(2);
-        s2.setName("João");
-        s2.setCourse("Administração");
-
-        Student s3 = new Student();
-        s3.setId(3);
-        s3.setName("Rogério");
-        s3.setCourse("Mecânica");
-
-        ArrayList<Student> lista = new ArrayList();
-        lista.add(s1);
-        lista.add(s2);
-        lista.add(s3);
-
-        return lista;
+    public List<Student> getStudents() {
+        return studentService.getStudents();
     }
 }
---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO TBL_STUDENT(name, course) VALUES ('Guilherme', 'Análise e Desenvolvimento de Sistemas')
+INSERT INTO TBL_STUDENT(name, course) VALUES ('Rogério', 'Mecatrônica Industrial')
+INSERT INTO TBL_STUDENT(name, course) VALUES ('Fernanda', 'Medicina Veterinária')
+INSERT INTO TBL_STUDENT(name, course) VALUES ('João', 'Mecânica')
