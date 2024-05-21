33dc10e51ee63bc6038d8c3eeb9a8124c5cf594f
guiiiividd <guiiiividd@gmail.com>
2024-05-21 11:24:39 -0300

    :sparkles: feat: GET, POST and DELETE endpoints

--- src/main/java/com/fatec/student/resources/StudentController.java
+++ src/main/java/com/fatec/student/resources/StudentController.java
@@ -3,7 +3,12 @@ package com.fatec.student.resources;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,5 +25,20 @@ public class StudentController {
     @GetMapping
     public List<Student> getStudents() {
         return studentService.getStudents();
+    }
+
+    @GetMapping("{id}")
+    public Student getStudentById(@PathVariable int id){
+        return studentService.getStudentById(id);
+    }
+
+    @DeleteMapping("{id}")
+    public void deleteStudentById(@PathVariable int id){
+        studentService.deleteStudentById(id);
+    }
+
+    @PostMapping
+    public Student saveStudent(@RequestBody Student student){
+        return studentService.saveStudent(student);
     }
 }
--- src/main/java/com/fatec/student/services/StudentService.java
+++ src/main/java/com/fatec/student/services/StudentService.java
@@ -8,13 +8,44 @@ import org.springframework.stereotype.Service;
 import com.fatec.student.entities.Student;
 import com.fatec.student.repositories.StudentRepository;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class StudentService {
-    
+
     @Autowired
     private StudentRepository studentRepository;
 
-    public List<Student> getStudents(){
-        return studentRepository.findAll();
+    public List<Student> getStudents() {
+        return this.studentRepository.findAll();
+    }
+
+    public Student getStudentById(int id) {
+        return this.studentRepository.findById(id).orElseThrow(
+            () -> new EntityNotFoundException("Aluno não cadastrado!")
+        );
+    }
+
+    public void deleteStudentById(int id){
+        if (this.studentRepository.existsById(id)) {
+            this.studentRepository.deleteById(id);
+        } else {
+            throw new EntityNotFoundException("Aluno não cadastrado!"); 
+        }
+    }
+    
+    public Student saveStudent(Student student){
+        return this.studentRepository.save(student);
+    }
+
+    public void updateStudent(int id, Student student){
+        try {
+            Student aux = this.studentRepository.getReferenceById(id);
+            aux.setCourse(student.getCourse());
+            aux.setName(student.getName());
+            this.studentRepository.save(aux);
+        } catch (EntityNotFoundException e) {
+            throw new EntityNotFoundException("Aluno não cadastrado!");
+        }
     }
 }
